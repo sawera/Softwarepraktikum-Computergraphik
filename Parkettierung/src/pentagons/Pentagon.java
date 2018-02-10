bbe17c73ee8c204c5b09813e983c2c871789e050
LennartKomander <komander.m@web.de>
2018-02-10 18:39:10 +0100

    Some more tests
    -Tested actual drawing of polygons
    -started to implement pentagon-superclass as polygon
    
    ist doch kniffliger als gedacht ~

---
+++ Parkettierung/src/pentagons/Pentagon.java
@@ -0,0 +1,24 @@
+/*
+ * Defines our average Pentagon using 2D Polygons.
+ */
+
+package pentagons;
+
+import java.awt.Point;
+import java.awt.Polygon;
+
+public class Pentagon extends Polygon{
+
+	int npoints = 5;	// cause its a pentagon
+	
+	float[] edges;
+
+	/**
+	 * @param start: relative position in a given grid
+	 * @param scale: size-parameter to somewhat ... make it bigger/smaller
+	 * @param rotation: ... u know
+	 */
+	public Pentagon(Point start, float scale, float rotation) {
+		
+	}
+}
